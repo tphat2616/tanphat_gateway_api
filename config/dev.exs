aac1dddb46db49623c4596449e042552a7face5f
Phat T.Nguyen <32483104+tphat2616@users.noreply.github.com>
2024-02-01 21:56:03 +0700

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -3,8 +3,8 @@ use Mix.Config
 # Configure your database
 config :tanphat_gateway_api, TanphatGatewayApi.Repo,
   username: "postgres",
-  password: "09098237720aA@",
-  database: "tphat2616_api",
+  password: "",
+  database: "",
   hostname: "51.79.165.79",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
